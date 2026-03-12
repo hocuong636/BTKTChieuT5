601e96528f4b59bcb86dfc47a1f2c92de22aa80f
Hồ Quốc Cường <hocuong636@gmail.com>
2026-03-12 15:08:22 +0700

    feat: change company name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       </div>
       <div id="footer">
         <p><a href="#">HOME PAGE</a> | <a href="#">ABOUT US</a> | <a href="#">BOUTQUETS</a> | <a href="#">SPECIALS</a> | <a href="#">NEWS</a> | <a href="#">CONTACTS</a><br />
-          Copyright &copy; Your Company Name | Design by <a href="http://freshtemplates.com/">Website Templates</a></p>
+          Copyright &copy; Cuong Company | Design by <a href="http://freshtemplates.com/">Website Templates</a></p>
       </div>
     </div>
   )
