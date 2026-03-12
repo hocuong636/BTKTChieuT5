00d58ec9c5e1010c343f456f275441ea02af1281
Hồ Quốc Cường <hocuong636@gmail.com>
2026-03-12 14:15:28 +0700

    feat: add logo and slogan to main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import logo from "../src/assets/image/logo.gif"
 function App() {
 
   return (
     <div id="container">
       <div id="topLine"></div>
+      <div id="logoPan"> <img src="images/logo.gif" width="192" height="92" alt="" id="logo" /> <img src="images/slogan.gif" width="297" height="46" alt="" id="slogan" /> </div>
     </div>
   )
 }
