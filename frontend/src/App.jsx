1e3e713ff4969d4e1dbcad539409ec34737a6bb2
Minh Quý <183045434+mquy27@users.noreply.github.com>
2026-03-12 14:56:52 +0700

    feat: footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,6 +43,10 @@ function App() {
           </div>
         </div>
       </div>
+      <div id="footer">
+        <p><a href="#">HOME PAGE</a> | <a href="#">ABOUT US</a> | <a href="#">BOUTQUETS</a> | <a href="#">SPECIALS</a> | <a href="#">NEWS</a> | <a href="#">CONTACTS</a><br />
+          Copyright &copy; Your Company Name | Design by <a href="http://freshtemplates.com/">Website Templates</a></p>
+      </div>
     </div>
   )
 }
